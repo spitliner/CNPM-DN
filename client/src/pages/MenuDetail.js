ec0d5314f277e4948b192ed390519f5a89985092
Hồ Đức Trí <tri.ho_dt_bd@hcmut.edu.vn>
2021-11-09 21:24:38 +0700

    menu detail

--- client/src/pages/MenuDetail.js
+++ client/src/pages/MenuDetail.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+import "./MenuDetail.css";
+import {FoodDetail} from "./Menu.js"
+
+function PostItem(props) {
+  return (
+    <div className="post-item">
+      <h2 className="post-name"> {props.food_name}</h2>
+      <form action="http://localhost:3000/menu">
+        <button class="post-button-BTM" type="submit">Back To Menu</button>
+      </form>
+      <div>
+        <img className="post-img"
+          src={props.image}
+        />
+      </div>
+      <h2 className="post-Ingredient">Ingredient</h2>
+      <p className="post-desc">{props.food_description}</p>
+      <h3 className="post-price">Prices: &nbsp; {props.food_price}$</h3>
+      <button className="post-button-ATC">Add To Cart</button>
+    </div>
+  )
+}
 
 class MenuDetail extends React.Component {
+
   render() {
     return (
       <React.Fragment>
-        <h1>Menu Detail</h1>
+        <div>
+          {
+            FoodDetail.map(food => (
+              <PostItem
+                key={food.id}
+                food_name={food.name}
+                image={food.imgUrl}
+                food_description={food.description}
+                food_price={food.pricePU}
+              />
+            ))
+          }
+        </div>
+
+
       </React.Fragment>
     );
   }
