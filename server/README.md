87ecc45ac4d4f5a1918d3caf13449e28e972ee53
dat.huynh11082001@hcmut.edu.vn <dat.huynh11082001@hcmut.edu.vn>
2021-11-11 22:53:13 +0700

    Update

--- server/README.md
+++ server/README.md
@@ -1,3 +1,129 @@
-# server
+### login:
 
+- Kiểm tra đăng nhập chưa:
 
+  - Request:
+
+    - method: GET
+    - url: server_url/api/logged_in
+    - data: {}
+    - withCredentials: true // Gởi request nhớ có dòng này tại server set cần yêu cầu chứng chỉ
+
+  - Response:
+    - status: 200
+    - data: {
+      loggedIn: true/false
+      message: mô tả chi tiết
+      }
+
+- doSubmit khi ấn nút login
+
+  - Request:
+
+    - method: POST
+    - url: server_url/api/login
+    - data: {
+      email: email
+      password: password
+      }
+    - withCredentials: true
+
+  - Response:
+    - status: 200
+    - data: {
+      success: true/false ứng với đăng nhập thành công hay thất bại
+      message: mô tả chi tiết
+      user: thông tin full về user, thất bại thì null
+      }
+
+# register:
+
+- Kiểm tra đăng nhập chưa: giống login
+
+- doSubmit khi ấn register:
+
+  - Request:
+
+    - method: GET
+    - url: server_url/api/register
+    - data: {
+      email: email
+      password: password
+      username: username
+      address: address
+      phone: phone
+      }
+    - withCredentials: true
+
+  - Response:
+    - status: 200
+    - data: {
+      success: true/false
+      message: Mô tả chi tiết
+      }
+
+# logout
+
+- Request:
+
+  - method: GET
+
+    - url: server_url/api/logout
+    - data: {}
+    - withCredentials: true
+
+- Response:
+  - status: 200
+  - data: {
+    success: true/false
+    message: mô tả chi tiết
+    }
+
+# change password
+
+- Request:
+
+  - method: POST
+
+    - url: server_url/api/change_password
+    - data: {
+      oldPassword
+      newPassword
+      }
+
+    - withCredentials: true
+
+- Response:
+  - status: 200
+  - data: {
+    success: true/false
+    message: mô tả chi tiết
+    }
+- Note:
+  Sau đó logout bắt đăng nhập lại do destroy session
+
+# change user info:
+
+## Tạm thời thì không cho đổi email, nếu muốn thì cũng có thể làm
+
+## Nếu mà không nhập thì gởi "" coi như không đổi
+
+- Request:
+
+  - method: POST
+
+    - url: server_url/api/change_user_info
+    - data: {
+      newUsername
+      newAddress
+      newPhone
+      }
+
+    - withCredentials: true
+
+- Response:
+  - status: 200
+    data: {
+    success: true/false
+    message: mô tả chi tiết
+    }
--- server/routes/index.js
+++ server/routes/index.js
@@ -16,7 +16,7 @@ router.post("/api/login", (req, res, next) => {
                 if (err) {
                     res.status(200).json({ message: err, success: false, user: null })
                 } else {
-                    res.status(200).json({ message: "Successfully Authenticated", success: true, user: req.user })
+                    res.status(200).json({ message: "Successfully Authenticated!", success: true, user: req.user })
                 }
             });
         }
@@ -24,9 +24,9 @@ router.post("/api/login", (req, res, next) => {
 });
 router.post("/api/register", (req, res) => {
     User.findOne({ email: req.body.email }, async(err, user) => {
-        if (err) {
+        if (err)
             res.status(200).json({ message: err, success: false });
-        }
+
         if (user)
             res.status(200).json({ message: "Email is already in use!", success: false });
         if (!user) {
@@ -46,4 +46,43 @@ router.post("/api/register", (req, res) => {
         }
     });
 });
+router.get("/logout", (req, res) => {
+    if (!req.isAuthenticated()) {
+        res.status(200).json({ success: false, message: "Incorrect flow! You are not logged in!" })
+    } else {
+        req.session.destroy(() => {
+            req.logout();
+        });
+        res.status(200).json({ success: false, message: "Logout successfully!" })
+    }
+});
+router.post("/change_password", (req, res) => {
+    var email = req.user.email;
+    User.findOne({ 'email': email }, function(err, user) {
+        if (err) return res.status(200).json({ success: false, message: err });
+        if (!user.validPassword(req.body.oldPassword)) {
+            return res.status(200).json({ success: false, message: "Incorect old password!" })
+        }
+        user.password = user.encryptPassword(req.body.newPassword);
+        user.save(function(err, result) {
+            if (err) res.status(200).json({ success: false, message: err });
+            else res.status(200).json({ success: true, message: "Successfully change passwod" });
+        })
+    })
+});
+router.post("/change_user_info", (req, res) => {
+    var email = req.user.email;
+    User.findOne({ 'email': email }, function(err, user) {
+        if (err) return res.status(200).json({ success: false, message: err });
+        if (req.body.newEmail) user.email = req.body.newEmail;
+        if (req.body.newAddress) user.address = req.body.newAddress;
+        if (req.body.newPhone) user.phone = req.body.newPhone;
+        user.save(function(err, result) {
+            if (err) res.status(200).json({ success: false, message: err });
+            else res.status(200).json({ success: true, message: "Successfully change user infomation" });
+        })
+    })
+})
+
+
 module.exports = router;
