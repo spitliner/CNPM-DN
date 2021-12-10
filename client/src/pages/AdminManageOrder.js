0f9a7a3ff90ef7fa030f396dfaf3e2d31b05f4b4
spitliner <73539619+spitliner@users.noreply.github.com>
2021-12-10 13:46:40 +0700

    add call to stat in admin managing order

--- client/src/pages/AdminManageOrder.js
+++ client/src/pages/AdminManageOrder.js
@@ -3,17 +3,18 @@ import { UserContext } from "../context/User";
 import ManageOrderFrame from "../components/ManageOrderFrame";
 import AdminManageOrderFrame from "../components/AdminManageOrderFrame";
 import "./AdminManageOrder.css";
+import RenderStat from "../components/stat.js";
 
 class AdminManageOrder extends React.Component {
   render() {
     return (
       <UserContext.Consumer>
         {({ currentLoginUser }) => (
+          <><RenderStat />
           <AdminManageOrderFrame
             currentLoginUser={currentLoginUser}
             menuItems={this.props.menuItems}
-            history={this.props.history}
-          />
+            history={this.props.history} /></>
         )}
       </UserContext.Consumer>
     );
